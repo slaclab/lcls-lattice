10bf5c77b10baeba5f582df7646707bb69867695
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-06-08 22:03:18 -0700

    tweak to slac2bmad expand_names

--- bmad/conversion/python/slac2bmad/xsif.py
+++ bmad/conversion/python/slac2bmad/xsif.py
@@ -69,6 +69,9 @@ FULLNAMES = {
 
 def expand_names(line):
     for k, v in FULLNAMES.items():
+        # Skip ones that are fine
+        if v in line.lower():
+            continue
         line = re.sub(k,v,line)
     return line
 
