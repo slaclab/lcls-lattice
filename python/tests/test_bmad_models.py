94808e003be412b66d95f882570213311bbfe3af
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2022-03-04 13:32:22 -0800

    test script and workflow

---
+++ python/tests/test_bmad_models.py
@@ -0,0 +1,33 @@
+from pytao import Tao
+import pytest
+import os
+
+mdir = os.path.expandvars("$LCLS_LATTICE/bmad/models")
+
+# Select models for testing. 
+MODELS = [
+    'cu_sxr',
+ #'hxr',
+ #'cu_spec',
+# 'lcls_complex',
+ 'sc_diag0',
+ 'cu_hxr',
+# 'cu_inj',
+ 'sc_dasel',
+ #'cu_linac',
+ 'sc_sxr',
+ 'sc_hxr',
+ 'sc_bsyd']
+
+
+def run_tao(model):
+    init = f"-init {mdir}/{model}/tao.init -noplot"
+    print(init)
+    tao = Tao(init)
+    return tao
+
+@pytest.mark.parametrize("model", MODELS)
+def test_model(model):
+    tao = run_tao(model)
+    name = tao.branch1(1,0)["name"]
+    assert name.lower() == model.lower()
