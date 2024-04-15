519f1a599cb7c4dc0cace94f911ffdf05f1fe25b
Michael Ehrlichman <mpe@slac.stanford.edu>
2024-04-15 15:53:11 -0700

    pytest for twiss comparisons

--- python/tests/test_twiss_comparison.py
+++ python/tests/test_twiss_comparison.py
@@ -1,42 +1,70 @@
 #!/bin/env python3
 
-comments = ['!','*','@','$','#']
+import pytest
+from subprocess import run, Popen, PIPE, STDOUT
+import os
+
+my_env = os.environ.copy()
+LCLS_LATTICE=my_env['LCLS_LATTICE']
+
+MODELS = [
+'sc_bsyd',
+'sc_sxr',
+'sc_hxr',
+# 'cu_sxr',
+# 'cu_spec',
+# 'sc_diag0',
+# 'cu_hxr',
+# 'cu_inj',
+# 'sc_dasel',
+ #'cu_linac',
+# 'sc_inj',
+]
+
+@pytest.mark.parametrize("model", MODELS)
+def test_exec_mad8s(model):
+  mad8s_commands = open(LCLS_LATTICE+'/mad/'+model.upper()+'_CI-Testing.mad8')
+  mad8s_result = run(['../mad8s'],cwd=LCLS_LATTICE+'/mad', stdin=mad8s_commands, capture_output=True, text=True)
+  assert os.path.exists(LCLS_LATTICE+'/mad/'+model.upper()+'_GUN_CI.twiss')
+
+@pytest.mark.parametrize("model", MODELS)
+def test_exec_mad8s(model):
+  bmad_result = run(['../../../lc_unit_test_bmad',model+'.lat.bmad'],cwd=LCLS_LATTICE+'/bmad/models/'+model, capture_output=True, text=True)
+  assert os.path.exists(LCLS_LATTICE+'/bmad/models/'+model+'/twiss.out')
 
 def parse_file(file_name):
-	data_lines = []
-	with open(file_name,'r') as f:
-		for line in f:
-			if line.lstrip()[0] not in comments:
-				data_lines.append(line.split())
-	return data_lines
+  data_lines = []
+  with open(file_name,'r') as f:
+    for line in f:
+      if line.lstrip()[0] not in comments:
+        data_lines.append(line.split())
+  return data_lines
 
-tests_pass = True
+comments = ['!','*','@','$','#']
 
-# test 1
-# Compare beta_x as the end of both the mad and bmad sc_bsyd (cathode to dump) line
+@pytest.mark.parametrize("model", MODELS)
+def test_bmad_mad8s_agreement(model):
+  # test 1
+  # Compare beta_x as the end of both the mad and bmad sc_bsyd (cathode to dump) line
+  tests_pass = True
 
-eps = 1e-6
+  eps = 1e-5
 
-bmad_data = parse_file('bmad/models/sc_bsyd/twiss.out')
-mad8_data = parse_file('mad/SC_BSYD_GUN_CI.twiss')
-	
-bmad_beta_x = float(bmad_data[-1][3])
-mad8_beta_x = float(mad8_data[-1][2])
+  bmad_data = parse_file(LCLS_LATTICE+'/bmad/models/'+model+'/twiss.out')
+  mad8_data = parse_file(LCLS_LATTICE+'/mad/'+model.upper()+'_GUN_CI.twiss')
+    
+  bmad_beta_x = float(bmad_data[-1][3])
+  mad8_beta_x = float(mad8_data[-1][2])
 
-test = abs((bmad_beta_x-mad8_beta_x) / (bmad_beta_x+mad8_beta_x) / 2) 
-print('Checking beta_y as SC_BSYD end ...')
-if (test < eps):
-  print('   pass!')
-else:
-  print('   fail! {}  {}'.format(float(bmad_beta_x), float(mad8_beta_x)))
-  tests_pass = False
+  test = abs((bmad_beta_x-mad8_beta_x) / (bmad_beta_x+mad8_beta_x) / 2) 
+  print(test<eps)
+  assert test < eps
 
 # test 2
 # Compare beta_x as the end of both the mad and bmad sc_hxr (cathode to dump) line
 
 #   under development
 
-if(tests_pass):
-  exit(0)
-else:
-  exit(1)
+#test_exec_mad8s()
+#test_exec_mad8s()
+#test_bmad_mad8s_agreement()
