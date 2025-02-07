221f599c54e236e523797916f08fb99fec971037
Michael Ehrlichman <mpe@slac.stanford.edu>
2025-02-06 17:25:38 -0800

    dev

--- python/scripts/oracle_upload/surveyDataTao.py
+++ python/scripts/oracle_upload/surveyDataTao.py
@@ -7,42 +7,43 @@ import numpy as np
 
 survey = []
 kws = {}
-kws['Beginning_Ele']   = {'madk':'MARK',
-                      'params':[0,0,0,0,'X1_LIMIT',0,0,0,0,0]} 
-kws['Multipole']   = {'madk':'MULT',
-                      'params':['L','K0L','K1L','K2L','X1_LIMIT','T0','K3L','T1','T2','T3']} 
-kws['Solenoid']    = {'madk':'SOLE',
-                      'params':['L',0,0,0,'X1_LIMIT',0,'KS',0,0,0]}
-kws['Lcavity']     = {'madk':'LCAV',
-                      'params':['L',0,0,0,'X1_LIMIT',0,'RF_FREQUENCY','VOLTAGE','PHI0',0]}
-kws['Instrument']  = {'madk':'INST',
-                      'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
-kws['ECollimator'] = {'madk':'ECOL',
-                      'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
-kws['RCollimator'] = {'madk':'ECOL',
-                      'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
-kws['Monitor']     = {'madk':'MONI',
-                      'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
-kws['HKicker']     = {'madk':'HKIC',
-                      'params':['L',0,0,0,'X1_LIMIT','KICK',0,0,0,0]}
-kws['VKicker']     = {'madk':'VKIC',
-                      'params':['L',0,0,0,'X1_LIMIT',0,'KICK',0,0,0]}
-kws['Quadrupole']  = {'madk':'QUAD',
-                      'params':['L',0,'K1',0,'X1_LIMIT','TILT',0,0,0,0]}
-kws['Sextupole']   = {'madk':'SEXT',
-                      'params':['L',0,0,'K2','X1_LIMIT','TILT',0,0,0,0]}
-kws['RBend']       = {'madk':'RBEN',
-                      'params':['L','ANGLE','K1','K2','X1_LIMIT','REF_TILT','E1','E2','H1','H2']}
-kws['SBend']       = {'madk':'SBEN',
-                      'params':['L','ANGLE','K1','K2','X1_LIMIT','REF_TILT','E1','E2','H1','H2']}
-kws['Taylor']      = {'madk':'MATR',
-                      'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
-kws['Wiggler']     = {'madk':'MATR',
-                      'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
-kws['Marker']      = {'madk':'MARK',
-                      'params':[0,0,0,0,0,0,0,0,0,0]}
-kws['Drift']       = {'madk':'DRIF',
-                      'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
+kws['Beginning_Ele']   = {'madk':'    ',
+                          'params':[0,0,0,0,0,0,0,0,0,0]} 
+kws['Multipole']       = {'madk':'MULT',
+                          'params':['L','K0L','K1L','K2L','X1_LIMIT','T0','K3L','T1','T2','T3']} 
+kws['Solenoid']        = {'madk':'SOLE',
+                          'params':['L',0,0,0,'X1_LIMIT',0,'KS',0,0,0]}
+kws['Lcavity']         = {'madk':'LCAV',
+                          'params':['L',0,0,0,'X1_LIMIT',0,'RF_FREQUENCY','VOLTAGE','PHI0',0]}
+kws['Instrument']      = {'madk':'INST',
+                          'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
+kws['ECollimator']     = {'madk':'ECOL',
+                          'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
+kws['RCollimator']     = {'madk':'ECOL',
+                          'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
+kws['Monitor']         = {'madk':'MONI',
+                          'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
+kws['HKicker']         = {'madk':'HKIC',
+                          'params':['L',0,0,0,'X1_LIMIT','KICK',0,0,0,0]}
+kws['VKicker']         = {'madk':'VKIC',
+                          'params':['L',0,0,0,'X1_LIMIT',0,'KICK',0,0,0]}
+kws['Quadrupole']      = {'madk':'QUAD',
+                          'params':['L',0,'K1',0,'X1_LIMIT','TILT',0,0,0,0]}
+kws['Sextupole']       = {'madk':'SEXT',
+                          'params':['L',0,0,'K2','X1_LIMIT','TILT',0,0,0,0]}
+kws['RBend']           = {'madk':'RBEN',
+                          'params':['L','ANGLE','K1','K2','X1_LIMIT','REF_TILT','E1','E2','H1','H2']}
+kws['SBend']           = {'madk':'SBEN',
+                          'params':['L','ANGLE','K1','K2','X1_LIMIT','REF_TILT','E1','E2','H1','H2']}
+kws['Taylor']          = {'madk':'MATR',
+                          'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
+kws['Wiggler']         = {'madk':'MATR',
+                          'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
+kws['Marker']          = {'madk':'MARK',
+                          'params':[0,0,0,0,0,0,0,0,0,0]}
+kws['Drift']           = {'madk':'DRIF',
+                          'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
+                          'params':['L',0,0,0,'X1_LIMIT',0,0,0,0,0]}
 
 skips = ['Patch','Pipe']
 for skip in skips:
@@ -65,7 +66,7 @@ for model in MODELS:
   tao = Tao(init_file=INITFILE[model], noplot=True)
   ix_eles = tao.lat_list('*', 'ele.ix_ele')
   suml = 0
-  for ix in ix_eles[1:]:
+  for ix in ix_eles[:-1]:
     ele_info = tao.ele_head(ix)
     ele_info.update(tao.ele_gen_attribs(ix))
 
@@ -93,6 +94,8 @@ for model in MODELS:
       madk = template['madk']
       params = template['params']
 
+    if name == 'BEGINNING':
+      name = 'initial'
     name_use = name.split('#',1)[0].upper()
     line = f'{madk:4s}{name_use:16s}'
 
@@ -112,6 +115,8 @@ for model in MODELS:
         line = line + f'{0:16.9E}'
       else:
         val = ele_info[param]
+        if key == 'Lcavity' and (param == 'RF_FREQUENCY' or param == 'VOLTAGE'):
+          val = val / 1e6
         #if len(val) == 0:
         #  print(f'lat_list returned empty list for key {key} param {param}')
         #  sys.exit(1)
