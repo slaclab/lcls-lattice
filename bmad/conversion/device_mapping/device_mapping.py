0aec29ed3d45d3e6bf8dab7f21017c1e2fe06bbe
Michael Ehrlichman <mpe@slac.stanford.edu>
2025-05-06 16:23:31 -0700

    converting to bare python files

---
+++ bmad/conversion/device_mapping/device_mapping.py
@@ -0,0 +1,132 @@
+import os
+from subprocess import run
+
+LCLS_LATTICE_ENV = os.environ['LCLS_LATTICE']
+assert LCLS_LATTICE_ENV != ''
+lcls_lat_check_1 = run(f'ls {LCLS_LATTICE_ENV}/bmad/conversion',shell=True,capture_output=True)
+assert lcls_lat_check_1.returncode == 0
+
+FACET2_LATTICE_ENV = '/home/mpe/DATA/new_repo/facet2-lattice'  #os.environ['FACET2_LATTICE']
+assert FACET2_LATTICE_ENV != ''
+facet2_lat_check_1 = run(f'ls {FACET2_LATTICE_ENV}/bmad/conversion',shell=True,capture_output=True)
+assert facet2_lat_check_1.returncode == 0
+
+import pandas as pd
+import json
+
+# Table extracted from SLACPROD Oracle Database
+MASTER = f'{LCLS_LATTICE_ENV}/bmad/conversion/from_oracle/lcls_elements.csv'
+
+df = pd.read_csv(os.path.expandvars(MASTER))
+# Remove empty
+df = df[['Element', 'Control System Name']].dropna()
+
+# Elements are unique
+MADNAMES = list(df['Element'])
+assert len(MADNAMES) == len(set(MADNAMES))
+# Control System Names are not
+DEVICENAMES = list(df['Control System Name'])
+assert len(DEVICENAMES) >= len(set(DEVICENAMES))
+
+# These devices have multiple elements - a mistake?
+series  = df.groupby('Control System Name')['Element'].apply(list)
+for i, val in series.items():
+    if len(val) > 1:
+        # Skip klystrons - these are expected to be duplicated
+        if not val[0].startswith('K'):
+            print(i, val)
+return i, series, val
+
+# dict for lookup
+DEVICE = dict(zip(MADNAMES, DEVICENAMES))
+json.dump(DEVICE, open('element_devices.json', 'w'))
+
+BDIR = f'{LCLS_LATTICE_ENV}/bmad/'
+
+# All models
+MODELS = [d for d in os.listdir(BDIR+'models/') if os.path.isdir(BDIR+'/models/'+d)]
+INITFILE = {model:f'{LCLS_LATTICE_ENV}/bmad/models/{model}/tao.init' for model in MODELS}
+for k,v, in INITFILE.items():
+    print(f'{k:<16}{v}')
+
+# Tack on FACET-II if availiable
+
+FDIR = f'{FACET2_LATTICE_ENV}/bmad/'
+
+if os.path.exists(FDIR):
+    print('Adding FACET-II')
+    model = 'f2_elec'
+    ifile = f'{FDIR}/models/{model}/tao.init'
+    if os.path.exists(ifile):
+        print(f'Adding {model} model')
+        MODELS.append(model)
+        INITFILE[model] = ifile
+
+from pytao import Tao
+
+def ele_names(model):
+    init = INITFILE[model]
+    print(f'{model}')
+    tao = Tao(f'-init {init} -noplot')
+    names = tao.cmd('python lat_list 1@0>>*|model ele.name')
+    return names
+
+def remove_superslaves(names):
+    return [x for x in names if '#' not in x]
+
+def write_devicenames(unames, filename):
+    my_names = remove_superslaves(unames)
+    lines = ['! ---------',
+             '! Device mapping derived from '+MASTER
+
+            ]
+    for name in my_names:
+        if name in DEVICE:
+            line = name+'[alias]='+ DEVICE[name]
+
+        else:
+            #continue
+            line = '! No device listed for: '+name
+        lines.append(line)    
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line+'\n')
+    print('Written:', filename)
+
+CU_FILE = f'{BDIR}/master/LCLScu_devicenames.bmad'
+CU_FILE_BAK = f'{BDIR}/master/LCLScu_devicenames-bak.bmad'
+os.rename(CU_FILE,CU_FILE_BAK)
+open(CU_FILE, 'a').close()  #make an empty file
+
+_models = ['cu_hxr', 'cu_sxr', 'cu_spec']
+_names = []
+for _m in _models:
+    print(_m)
+    _names += ele_names(_m)
+_unames = sorted(list(set(_names)))
+
+write_devicenames(_unames, CU_FILE)
+
+SC_FILE = f'{BDIR}/master/LCLSsc_devicenames.bmad'
+
+_models = ['sc_hxr', 'sc_sxr', 'sc_diag0', 'sc_bsyd', 'sc_dasel']
+_names = []
+for _m in _models:
+    print(_m)
+    _names += ele_names(_m)
+_unames = sorted(list(set(_names)))
+
+write_devicenames(_unames, SC_FILE)
+
+os.environ['FACET2_LATTICE'] = '/home/mpe/DATA/new_repo/facet2-lattice/'
+if os.path.exists(FDIR):
+    F2_FILE = f'{FDIR}/master/FACET2e_devicenames.bmad'
+    _models = ['f2_elec']
+    _names = []
+    for _m in _models:
+        print(_m)
+        _names += ele_names(_m)
+    _unames = sorted(list(set(_names)))
+
+    write_devicenames(_unames, F2_FILE)
+
---
+++ bmad/conversion/slac_to_bmad.py
@@ -0,0 +1,301 @@
+#!/bin/env python3
+
+import os
+from subprocess import run
+
+LCLS_LATTICE_ENV = os.environ['LCLS_LATTICE']
+assert LCLS_LATTICE_ENV != ''
+lcls_lat_check_1 = run(f'ls {LCLS_LATTICE_ENV}/bmad/conversion',shell=True,capture_output=True)
+assert lcls_lat_check_1.returncode == 0
+print(f'{LCLS_LATTICE_ENV=}')
+
+BMAD_ENV = os.environ['ACC_ROOT_DIR']
+assert BMAD_ENV != ''
+bmad_env_check_1 = run(f'ls {BMAD_ENV}/util/dist_source_me',shell=True,capture_output=True)
+assert bmad_env_check_1.returncode == 0
+print(f'{BMAD_ENV=}')
+
+WORK_DIR = LCLS_LATTICE_ENV + '/bmad/conversion/work'
+TEMP_DIR = LCLS_LATTICE_ENV + '/bmad/conversion/temp'
+DEST_DIR = os.path.expandvars('$LCLS_LATTICE/bmad/master/')
+print(f'{WORK_DIR=}')
+print(f'{TEMP_DIR=}')
+
+
+# Patch in the slac2bmad package
+import sys
+sys.path.append(f'{LCLS_LATTICE_ENV}/bmad/conversion/python')
+
+from slac2bmad.xsif import prepare_xsif, remove_comment_blocks, replace_set, replace_set_commands, fix_matrix, expand_names, fix_names, unfold_comments, fold_comments
+from slac2bmad.desplit import desplit_eles, desplit_ele
+from slac2bmad.replace import replace_element, replace_eles
+from slac2bmad.bmad import finalize_bmad
+
+from glob import glob
+import shutil
+
+import json
+
+
+INCLUDE_DEFERRED = False
+
+# Test replacements
+#replace_set('SET,  afa, afa, 1a')
+#fix_matrix('RM(3,4)')  
+#expand_names('  afa APER BLMO')
+#fix_names(['sfafasfa safa APER RM(1,2)'])
+
+# Test comment unfolding
+#def test():
+#    L0 = ['123\n', '123    !comment\n', '   \n', '  !simple comment\n', '123!456#789\n']
+#    L1 = unfold_comments(L0)
+#    L2 = fold_comments(L1)
+#    print(L0)
+#    print(L1)
+#    print(L2)
+#test()
+#return (test,)
+
+# Test desplitting
+#line0 = 'qsx16_full: line = (qsx16, xcsx16, ycsx16, qsx16)'    
+#line1 = 'qsx16_full: line = (qsx16,  qsx16a)'  
+#line2 = 'qsx16_full: line = (qsx16)'  
+#line3 = 'pssxh999_full: line = (pssxh999a,stuff,pssxh999b)'
+#print(desplit_ele(line3,exclude_strs=['pssxh']))
+#desplit_ele('WIG2H_full : LINE=(WIG2H1,YCWIGH,WIG2H2)')
+#desplit_eles(['fafaa', line0])
+
+
+NEWELES = {}
+NEWELES['umasxh'] = """
+!------- SXR Undulator -------
+my_umasxh_k = 5.0
+umasxh: wiggler, 
+        type = "VGHPU",
+        L_period = 0.039, 
+        n_period = 87, 
+        b_max = my_umasxh_k * 2*pi*m_electron / (c_light * 0.039), 
+        L = 87*0.039, 
+        ds_step = 0.039*10
+
+umasxh[L] = umasxh[L]/2 ! Will be doubled in desplitting process. 
+!---------------------------------
+"""
+NEWELES['umahxh'] = """
+!------- HXR Undulator -------
+my_umahxh_k = 2.0
+umahxh: wiggler, 
+        type = "HGVPU",
+        L_period = 0.026, 
+        n_period = 129, 
+        b_max = my_umahxh_k * 2*pi*m_electron / (c_light * 0.026), 
+        L = 129*0.026, 
+        tilt=pi/2,
+        ds_step = 0.026*10
+
+umahxh[L] = umahxh[L]/2 ! Will be doubled in desplitting process. 
+!---------------------------------
+    """
+NEWELES['pssxh'] = """
+!------- SXR Phase Shifter -------
+!
+! B_max = 2pi/lambda * sqrt(2*PHASE_INTEGRAL / L)
+! 
+pssxh_phase_integral = 3814e-9  !T^2 m^3, maximum, from: T^2mm^3 (180-3814)
+pssxh_L        = 0.0825   ! m 
+pssxh_L_period = 0.075 ! m 
+pssxh: wiggler, type = "phase shifter", 
+    L = pssxh_L,
+    b_max = 2*pi / pssxh_L_period * sqrt(2 * pssxh_phase_integral / pssxh_L  ),
+    n_period = 1
+pssxh[L] = pssxh[L]/2 ! Will be doubled in desplitting process. 
+!---------------------------------
+"""
+NEWELES['pshxh'] = """
+!------- HXR Phase Shifter -------
+!
+! B_max = 2pi/lambda * sqrt(2*PHASE_INTEGRAL / L)
+! 
+pshxh_phase_integral = 490e-9  !T^2 m^3, maximum, from: T^2mm^3 (80-490)
+pshxh_L        = 0.0495 ! m 
+pshxh_L_period = 0.045 ! m 
+pshxh: wiggler, type = "phase shifter", 
+    L = pshxh_L,
+    b_max = 2*pi / pshxh_L_period * sqrt(2 * pshxh_phase_integral / pshxh_L  ),
+    n_period = 1
+pshxh[L] = pshxh[L]/2 ! Will be doubled in desplitting process. 
+!---------------------------------
+"""
+#-----------------
+# XLEAP-II wigglers
+NEWELES['umxl1h'] = """
+!------- XLEAP-II wigglers -------
+umxl0h: wiggler, 
+        type = "LCLS-I",
+        L_period = 0.555, 
+        n_period = 6, 
+        b_max = 0, ! = K * 2*pi*m_electron / (c_light * 0.55), 
+        L = 6*0.555
+        !ds_step = 0.55*10
+
+umxl0h[L] = umxl0h[L]/2 ! Will be doubled in desplitting process. 
+!---------------------------------
+
+umxl1h: umxl0h
+
+"""
+# Inherit from umxl0h
+NEWELES['umxl2h'] = """
+umxl2h: umxl0h
+"""
+NEWELES['umxl3h'] = """
+umxl3h: umxl0h
+"""
+NEWELES['umxl4h'] = """
+umxl4h: umxl0h
+"""
+# This needs to be extended
+NEWELES['duqxl'] = """
+! Extend to account for real WIGGLER elements for XLEAP
+duqxl: drift, L = 0.2166 + 0.03
+"""
+
+
+# CU only replacements
+CU_NEWELES = {}
+CU_NEWELES['lh_und'] = """
+!------- Laser Heater Undulator for Copper Linac -------
+my_lh_und_k = 1.38523
+lh_und: wiggler, 
+        type = "laser_heater_undulator",
+        L_period = 0.054, 
+        n_period = 10, 
+        b_max = my_lh_und_k * 2*pi*m_electron / (c_light * 0.054), 
+         L = 10*0.054 ! Was: 0.506263, 
+        ds_step = 0.054
+
+lh_und[L] = lh_und[L]/2 ! Will be doubled in desplitting process. 
+!---------------------------------
+    """
+CU_NEWELES['dh03a'] = """
+! Shorten so that lh_und has an integer number of poles
+dh03a: drift, l = 0.09290825 - ( 10*0.054 - 0.506263 ) /2, type = "CSR"
+"""
+CU_NEWELES['dh03b'] = """
+! Shorten so that lh_und has an integer number of poles
+dh03b: drift, l = 0.08401830- ( 10*0.054 - 0.506263 ) /2, type = "CSR"
+"""
+# Append json replacements to CU_NEWELES
+CU_LINAC_REPLACEMENTS = json.load(open('bmad/conversion/replacements/good_cu_linac_replacements.json'))
+for name, replace in CU_LINAC_REPLACEMENTS.items():
+    CU_NEWELES[name.lower()+'_full'] = replace
+# Append deferred replacements to CU_NEWELES
+if INCLUDE_DEFERRED:
+    CU_NEWELES.update(json.load(open('bmad/conversion/replacements/deferred_cu_replacements.json')))
+
+
+# SC Only replacements
+SC_NEWELES = {}
+SC_NEWELES['umhtr'] = """
+!------- Laser Heater Undulator for SC Linac -------
+my_umhtr_k = 0.960143
+
+umhtr: wiggler, 
+        type = "laser_heater_undulator",
+        L_period = 0.054, 
+        n_period = 10, 
+        b_max = my_umhtr_k * 2*pi*m_electron / (c_light * 0.054), 
+        L = 10*0.054 ! Was: 0.506263, 
+        ds_step = 0.054
+
+umhtr[L] = umhtr[L]/2 ! Will be doubled in desplitting process. 
+!---------------------------------
+    """
+SC_NEWELES['dh02c'] = """
+! Shorten so that umhtr has an integer number of poles
+dh02c: drift, l = 0.2795065 - ( 10*0.054 - 0.506263 ) /2 , type = "CSR" !0.297036
+"""
+SC_NEWELES['dh02d'] = """
+! Shorten so that umhtr has an integer number of poles
+dh02d: drift, l = 0.2724707 - ( 10*0.054 - 0.506263 ) /2, type = "CSR" !0.2900002
+"""
+# Not needed. Desplitting handles cavities now.
+# Add these repalcements
+#SC_LINAC_REPLACEMENTS = json.load(open('replacements/good_sc_linac_replacements.json'))
+#for name, replace in SC_LINAC_REPLACEMENTS.items():
+#    SC_NEWELES[name.lower()+'_full'] = replace
+# Append deferred replacements to SC_NEWELES
+if INCLUDE_DEFERRED:
+    SC_NEWELES.update(json.load(open('bmad/conversion/replacements/deferred_sc_replacements.json')))
+
+def all_replacements(master_file):
+    dat = {}
+    dat.update(NEWELES)
+    if master_file.startswith('CU_'):
+        print('CU replacements')
+        dat.update(CU_NEWELES)
+        return dat
+    elif master_file.startswith('SC_'):
+        print('SC replacements')
+        dat.update(SC_NEWELES)
+        return dat
+    else:
+        raise 
+
+#Cleanup from previous conversion run
+run(f'rm -rf {TEMP_DIR}',shell=True)
+run(f'mkdir {TEMP_DIR}',shell=True)
+#run('rm -f *.xsif *.bmad *.digested*',shell=True)
+run(f'cp $LCLS_LATTICE/mad/*.xsif {TEMP_DIR}',shell=True)
+
+#Identify and prepare all xsif files
+XSIF_FILES=[f for f in os.listdir(TEMP_DIR) if f.endswith('.xsif')]
+print(XSIF_FILES)
+for f in XSIF_FILES:
+    prepare_xsif(f'{TEMP_DIR}/{f}', save=False)
+
+#Identify Cu and SC xsif files
+CU_MASTERS = [f for f in os.listdir('mad') if f.startswith('CU_') and f.endswith('xsif')]
+SC_MASTERS = [f for f in os.listdir('mad') if f.startswith('SC_') and f.endswith('xsif')]
+print(f'{CU_MASTERS=}')
+print(f'{SC_MASTERS=}')
+
+
+exclude_strs = ['BUN1B','WIGX','UMXL','LH_UND','UMHTR','UMASX','UMAHX','PSSX','PSHX']
+shadows = ['umasxh','umahxh','pssxh','pshxh','umxl1h','umxl2h','umxl3h','umxl4h',
+           'duqxl','lh_und','dh03a','dh03b','umhtr','dh02c','dh02d']
+
+def process_master(master):
+    print(f'Converting {master}')
+
+    shutil.copytree(TEMP_DIR, WORK_DIR, dirs_exist_ok=True)
+
+    # New method
+    SCRIPT = f'python $ACC_ROOT_DIR/util_programs/mad_to_bmad/mad8_to_bmad.py --no_prepend_vars -f {master}'
+
+    res = run(SCRIPT, shell=True, cwd=WORK_DIR)
+
+    assert res.returncode == 0
+
+    BMAD_FILES=glob(WORK_DIR+'/*bmad')
+
+    REPLACEMENTS = all_replacements(master)
+
+    for f in BMAD_FILES:
+        finalize_bmad(f, replacements=REPLACEMENTS, verbose=False, exclude_strs=exclude_strs, shadows=shadows)  
+
+    print(f'    Copying all to {DEST_DIR}')
+    for f in BMAD_FILES:
+        #print(f'copying {f} to {DEST}')
+        shutil.copy(f, DEST_DIR)
+#process_master('SC_SXR.xsif')
+
+for _m in CU_MASTERS:
+    process_master(_m)
+
+for _m in SC_MASTERS:
+    process_master(_m)
+
+#cleanup working areas
+run(f'rm -r {TEMP_DIR}',shell=True)
+run(f'rm -r {WORK_DIR}',shell=True)
