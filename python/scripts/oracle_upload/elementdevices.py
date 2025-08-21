cd7850032a30284b8c09db723e38afeec6b7edcc
Michael P. Ehrlichman <mpe@slac.stanford.edu>
2025-08-20 17:24:51 -0700

    python script to make the elementdevices files

---
+++ python/scripts/oracle_upload/elementdevices.py
@@ -0,0 +1,71 @@
+#!/bin/env python3
+
+import os
+import oracledb
+from pathlib import Path
+from datetime import datetime
+import shutil
+
+SQL1="""
+select unique element, epics_device_name, linacz_m from V_lcls_elements_report_public 
+where 
+(ELEMENT_TYPE='MAD' OR FIRST_SOURCED_FROM != 'CABLES') 
+AND ACTIVE_FLAG='A' 
+AND BEAMLINE_ID IN (11)
+order by (linacz_m)
+"""
+
+def get_sql2(eleids):
+    eleids_str = ",".join(f"'{x[:-1]}'" for x in eleids)
+    return f"""
+            select element, epics_device_name from V_lcls_elements_report_public 
+            where 
+            (ELEMENT_TYPE='MAD' OR FIRST_SOURCED_FROM != 'CABLES') 
+            AND ACTIVE_FLAG='A' 
+            AND BEAMLINE_ID IN (11)
+            AND element in ({eleids_str})
+            """
+
+oracledb.init_oracle_client(lib_dir=os.environ.get("ORACLE_HOME"))
+conn = oracledb.connect(
+    user = "LCLS_INFRASTRUCTURE",
+    password = "35G2!a65bba74e5b",
+    dsn=os.environ.get("TWO_TASK", "SLACPROD"), 
+)
+
+# test database connectivity
+#cur.execute("select sysdate from dual")
+#print("Connected. DB time:", cur.fetchone()[0])
+
+src = Path('elementdevices.dat')
+if src.is_file():
+    print('foo')
+    ts = datetime.now().strftime("%Y%m%dT%H%M%S")
+    dst = src.with_name(f"{src.name}-priorto-{ts}.log")
+    shutil.copyfile(src, dst)
+
+with conn.cursor() as cur, open('elementdevices.dat','w') as f:
+    cur.execute(SQL1)
+    for row in cur:
+        epics_device_name = row[1] if "NO EPICS NAME" not in row[1] else '-'
+        f.write(f'{row[0]} {epics_device_name}\n')
+
+def sbend_devices(survey_file, out_file):
+    with conn.cursor() as cur, open(survey_file,'r') as fin, open(out_file,'w') as fout:
+        eleids=[]
+        for line in fin:
+            if line.startswith('SBEN'):
+                eleids.append(line.split()[0][4:])
+        cur.execute(get_sql2(eleids))
+        out_lst = []
+        for row in cur:
+            epics_device_name = row[1] if "NO EPICS NAME" not in row[1] else '-'
+            out_lst.append(f'{row[0]} {epics_device_name}')
+        out_lst = sorted(set(out_lst))  #replicates sort -u
+        for ele in out_lst:
+            fout.write(f'{ele}\n')
+
+sbend_devices('LCLS2cuH_survey.tape', 'elementdevices_sbends_cuH.dat')
+sbend_devices('LCLS2cuS_survey.tape', 'elementdevices_sbends_cuS.dat')
+
+conn.close()
