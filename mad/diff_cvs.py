b054710f7c134b0afb823a8d2ef0c013c09d5b18
Michael Ehrlichman <mpe@slac.stanford.edu>
2025-12-15 15:21:00 -0800

    release tools

---
+++ mad/diff_cvs.py
@@ -0,0 +1,54 @@
+#!/bin/env python3
+
+import sys
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+
+with open(file1,'r') as f1:
+  n1 = 0
+  for line in f1:
+    n1 += 1
+with open(file2,'r') as f2:
+  n2 = 0
+  for line in f2:
+    n2 += 1
+  
+print(f'Lines in file 1: {n1}')
+print(f'Lines in file 2: {n2}')
+
+#rel_tol = 5e-8
+rel_tol = 1e-6
+
+veto_cols=[48,49,] #[6,50,]  #columns are zero-indexed
+
+with open(file1,'r') as f1, open(file2,'r') as f2:
+  header1_1 = f1.readline() 
+  header1_2 = f1.readline() 
+  header2_1 = f2.readline() 
+  header2_2 = f2.readline() 
+  for i in range(min(n1,n2)):
+    line1 = f1.readline().strip().split(',')
+    line2 = f2.readline().strip().split(',')
+    for j,pair in enumerate(zip(line1,line2)):
+      if j in veto_cols:
+        continue
+      if is_number(pair[0]) and is_number(pair[1]):
+        if pair[0] != pair[1]:
+          num1 = float(pair[0])
+          num2 = float(pair[1])
+          abs_diff = num1-num2
+          rel_diff = abs(num1-num2)/(abs(num1)+abs(num2))/2.0 
+          if rel_diff > rel_tol:
+            print(f'fail at {i+3} i={line1[0]}, j={j}: {pair} {rel_diff} {abs_diff}')
+            #print(f'    {num1=}   {num2=}')
+      else:
+        if pair[0].strip() != pair[1].strip():
+          print(f'fail at {i+3} i={line1[0]}, j={j}: {pair}')
---
+++ mad/diff_cvs_2.0.py
@@ -0,0 +1,74 @@
+#!/bin/env python3
+
+import sys
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+
+def parse_cvs(file):
+  cvs_dict = {}
+  with open(file,'r') as f:
+    header = f.readline() 
+    header = f.readline() 
+    for line in f:
+      line_lst = line.strip().split(',')
+      key = line_lst[3]
+      if key in cvs_dict:
+        sys.exit('key collision detected')
+      if key == 'ELEMENT':
+        break
+      cvs_dict[key] = line_lst
+  return cvs_dict
+
+data1 = parse_cvs(file1)
+data2 = parse_cvs(file2)
+  
+print(f'Lines in file 1: {len(data1)}')
+print(f'Lines in file 2: {len(data2)}')
+
+print("In file 1, but not in file 2:")
+got_one = False
+for key in data1.keys() - data2.keys():
+  got_one = True
+  print(key) 
+if not got_one:
+  print("   None!")
+
+print("In file 2, but not in file 1:")
+got_one = False
+for key in data2.keys() - data1.keys():
+  got_one = True
+  print(key) 
+if not got_one:
+  print("   None!")
+
+#rel_tol = 5e-8
+rel_tol = 1e-6
+
+veto_cols=[48,49,] #[6,50,]  #columns are zero-indexed
+
+print("Comparing common elements:")
+for key in data1.keys() & data2.keys():
+  data1_ = data1[key]
+  data2_ = data2[key]
+  for j,pair in enumerate(zip(data1_,data2_)):
+    if j in veto_cols:
+      continue
+    if is_number(pair[0]) and is_number(pair[1]):
+      if pair[0] != pair[1]:
+        num1 = float(pair[0])
+        num2 = float(pair[1])
+        abs_diff = num1-num2
+        rel_diff = abs(num1-num2)/(abs(num1)+abs(num2))/2.0 
+        if rel_diff > rel_tol:
+          print(f'fail for {key}, j={j}: {pair} {rel_diff} {abs_diff}')
+    else:
+      if pair[0].strip() != pair[1].strip():
+        print(f'fail for {key}: j={j}, {pair}')
