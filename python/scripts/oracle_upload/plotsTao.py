71e3d5b4f8da6d2c39310ccf457384b5e1dc6fe9
Michael P. Ehrlichman <mpe@slac.stanford.edu>
2026-03-19 18:27:11 -0700

    makefile dev

--- python/scripts/oracle_upload/plotsTao.py
+++ python/scripts/oracle_upload/plotsTao.py
@@ -22,7 +22,8 @@ if LCLS_LATTICE_ENV is None:
   sys.exit(1)
 
 BDIR = f'{LCLS_LATTICE_ENV}/bmad/'
-MODELS=['sc_sxr_beam0', 'sc_hxr_beam0',]
+MODELS=['sc_sxr_beam0', 'sc_hxr_beam0','sc_bsyd_beam0','sc_diag0_beam0','sc_dasel_beam0']
+MODELS=['cu_sxr','cu_hxr']
 #MODELS=['sc_sxr_beam0','sc_hxr_beam0','sc_bsyd_beam0','sc_diag0_beam0','sc_dasel_beam0','cu_sxr','cu_hxr']
 #MODELS=['sc_sxr','sc_hxr','sc_bsyd','sc_diag0','sc_dasel','cu_sxr','cu_hxr']
 LATFILE = {}
@@ -35,12 +36,20 @@ for model in MODELS:
   print(f'model {model}')
   with PdfPages(f'{model}_plots.pdf') as pdf:
     tao = Tao(lattice_file=LATFILE[model], plot_file="tao_plot.init", plot="mpl")
-    tao.update_plot_shapes("quadrupole", type_label="name", layout=True, floor=True)    
+    #tao.update_plot_shapes("quadrupole", type_label="name", layout=True, floor=True)    
     #tao.plot("energy", save=f'{model}_twiss.png', include_layout=True, ylim=(0,5e9))
-    tao.plot("energy", include_layout=True, ylim=(0,5e9))
-    fig = plt.gcf()
-    pdf.savefig(fig)
-    plt.close(fig)
+
+    if model in ['sc_sxr_beam0', 'sc_hxr_beam0','sc_bsyd_beam0','sc_dasel_beam0']:
+        tao.plot("energy", include_layout=True, ylim=(0,5e9))
+        fig = plt.gcf()
+        pdf.savefig(fig)
+        plt.close(fig)
+
+    if model in ['cu_sxr','cu_hxr']:
+        tao.plot("energy", include_layout=True, ylim=(0,10e9))
+        fig = plt.gcf()
+        pdf.savefig(fig)
+        plt.close(fig)
     
     tao.plot("beta", include_layout=True)
     fig = plt.gcf()
@@ -52,4 +61,15 @@ for model in MODELS:
     pdf.savefig(fig)
     plt.close(fig)
 
+    if model in ['sc_sxr_beam0','sc_hxr_beam0']:
+        tao.plot("beta", include_layout=True, xlim=(3420,3700), ylim=(0,100))
+        fig = plt.gcf()
+        pdf.savefig(fig)
+        plt.close(fig)
+
+    if model in ['cu_sxr','cu_hxr']:
+        tao.plot("beta", include_layout=True, xlim=(1375,1680), ylim=(0,160))
+        fig = plt.gcf()
+        pdf.savefig(fig)
+        plt.close(fig)
 
